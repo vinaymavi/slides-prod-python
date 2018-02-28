d27a12e04f91002018cc39f4369f88430ae67c88
VinayMavi <vinaymavi@gmail.com>
2018-02-28 23:44:20 +0530

    QuestionSet, Question, Options hierachy.

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from models import Greeting
+from model.question_set import QuestionSet, Question, Option
+
 # Register your models here.
 
 
 admin.site.register(Greeting)
+admin.site.register(QuestionSet)
+admin.site.register(Question)
+admin.site.register(Option)
